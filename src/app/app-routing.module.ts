4d7cfab369cb8c174abfec82702f57f89a1025eb
Kiryl <kiryl.yushkevich@gmail.com>
2020-04-03 15:40:21 +0300

    feat(routing): added routing for site

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { WebComponent } from './web/web.component';
+import { HomeComponent } from './web/home/home.component';
+import { ArticlesComponent } from './web/articles/articles.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HomeDashboardComponent } from './dashboard/home-dashboard/home-dashboard.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: WebComponent, children: [
+    { path: '', component: HomeComponent },
+    { path: 'articles', component: ArticlesComponent }
+  ]},
+  { path: 'dashboard', component: DashboardComponent, children: [
+    { path: '', component: HomeDashboardComponent },
+    { path: 'articles', component: ArticlesComponent },
+    { path: 'users', component: ArticlesComponent }
+  ]}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WebComponent } from './web/web.component';
+import { HomeComponent } from './web/home/home.component';
+import { ArticlesComponent } from './web/articles/articles.component';
+import { HeaderComponent } from './web/header/header.component';
+import { FooterComponent } from './web/footer/footer.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HomeDashboardComponent } from './dashboard/home-dashboard/home-dashboard.component';
+import { UsersDashboardComponent } from './dashboard/users-dashboard/users-dashboard.component';
+import { ArticlesDashboardComponent } from './dashboard/articles-dashboard/articles-dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WebComponent,
+    HomeComponent,
+    ArticlesComponent,
+    HeaderComponent,
+    FooterComponent,
+    DashboardComponent,
+    HomeDashboardComponent,
+    UsersDashboardComponent,
+    ArticlesDashboardComponent,
   ],
   imports: [
+    MDBBootstrapModule.forRoot(),
     BrowserModule,
     AppRoutingModule
   ],
