444c46f08c894843d05e6d577efcc604c4d86d80
Kiryl <kiryl.yushkevich@gmail.com>
2020-04-08 00:33:28 +0300

    feat(dashboard-article): added table of articles added delete article form table

--- src/app/dashboard/articles-dashboard/articles-dashboard.component.ts
+++ src/app/dashboard/articles-dashboard/articles-dashboard.component.ts
@@ -1,15 +1,64 @@
-import { Component, OnInit } from '@angular/core';
+import { MdbTablePaginationComponent, MdbTableDirective } from 'angular-bootstrap-md';
+
+import { Component, OnInit, ViewChild, HostListener, AfterViewInit, ChangeDetectorRef } from '@angular/core';
+
+import { ToastrService } from 'ngx-toastr';
+
+import { Article } from 'src/app/data/article';
+import { ArticleService } from 'src/app/shared/service/atricle.service';
 
 @Component({
   selector: 'app-articles-dashboard',
   templateUrl: './articles-dashboard.component.html',
   styleUrls: ['./articles-dashboard.component.scss']
 })
-export class ArticlesDashboardComponent implements OnInit {
+export class ArticlesDashboardComponent implements OnInit, AfterViewInit {
 
-  constructor() { }
+  @ViewChild(MdbTablePaginationComponent, { static: true }) mdbTablePagination: MdbTablePaginationComponent;
+  @ViewChild(MdbTableDirective, { static: true }) mdbTable: MdbTableDirective
+  private errorMessage: string;
+  articles: Article[];
+  article: Article;
+  previous: any = [];
+  headElements = ['Изображение', 'ID', 'Название', 'Содержание', 'Дата', ' '];
 
-  ngOnInit(): void {
+  constructor(private cdRef: ChangeDetectorRef, private service: ArticleService, private toastr: ToastrService) { }
+
+  ngOnInit() {
+
+    this.getArticles();
+
+    this.mdbTable.setDataSource(this.articles);
+    this.articles = this.mdbTable.getDataSource();
+    this.previous = this.mdbTable.getDataSource();
   }
 
+  ngAfterViewInit() {
+    this.mdbTablePagination.setMaxVisibleItemsNumberTo(5);
+
+    this.mdbTablePagination.calculateFirstItemIndex();
+    this.mdbTablePagination.calculateLastItemIndex();
+    this.cdRef.detectChanges();
+  }
+
+  getArticles(): void {
+    this.service.getArticles()
+                .subscribe(
+                  articles => this.articles = articles,
+                  error => this.errorMessage = error
+                );
+  }
+
+  // DELETE article
+  delArticle(id:any) {
+    this.service.delArticle(id)
+        .subscribe(
+            res => {
+                this.toastr.error('Статья удалена');
+                this.service.getArticles()
+                    .subscribe(article => this.articles = article)
+            },
+            error => this.errorMessage = error,
+        )
+  }
 }
