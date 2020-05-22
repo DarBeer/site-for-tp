e419e612f3968851b8d8e40fd1814ddd61ff6905
Kiryl <kiryl.yushkevich@gmail.com>
2020-05-22 15:11:58 +0300

    changed port to 80

--- bs-config.js
+++ bs-config.js
@@ -2,12 +2,12 @@ var proxyMiddleware = require('http-proxy-middleware');
 var fallbackMiddleware = require('connect-history-api-fallback');
 
 module.exports = {
-    port: 3000,
+    port: 80,
     server: {
         baseDir: './dist/site-for-tp/',
         middleware: {
             1: proxyMiddleware('/api', {
-                target: 'tp.polessu.by:3000',
+                target: 'tp.polessu.by:80',
                 changeOrigin: true   // for vhosted sites, changes host header to match to target's host
             }),
 
